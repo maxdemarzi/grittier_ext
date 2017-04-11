8ae5badbbccb4ceed69eeb61094d30fabf5438f5
Max De Marzi <maxdemarzi@hotmail.com>
2017-04-10 23:57:16 -0500

    removing redundant time artifacts

--- src/main/java/com/maxdemarzi/timeline/Timeline.java
+++ src/main/java/com/maxdemarzi/timeline/Timeline.java
@@ -11,12 +11,15 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
 import java.time.LocalDateTime;
-import java.time.ZoneId;
 import java.time.ZoneOffset;
-import java.time.format.DateTimeFormatter;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Map;
 
 import static com.maxdemarzi.Properties.*;
+import static com.maxdemarzi.Time.dateFormatter;
+import static com.maxdemarzi.Time.utc;
 import static com.maxdemarzi.likes.Likes.userLikesPost;
 import static com.maxdemarzi.posts.Posts.getAuthor;
 import static com.maxdemarzi.posts.Posts.userRepostedPost;
@@ -26,10 +29,6 @@ import static java.util.Collections.reverseOrder;
 public class Timeline {
 
     private static final ObjectMapper objectMapper = new ObjectMapper();
-    private static final ZoneId utc = TimeZone.getTimeZone("UTC").toZoneId();
-    private static final DateTimeFormatter dateFormatter = DateTimeFormatter
-            .ofPattern("yyyy_MM_dd")
-            .withZone(utc);
 
     @GET
     public Response getTimeline(@PathParam("username") final String username,
